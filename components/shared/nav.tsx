e9ccea7d1aea5b83375bf771f527d58a7e70e5bd
g00gol <jackeyyang.work@gmail.com>
2025-06-21 02:51:12 -0400

    add confetti

--- components/shared/nav.tsx
+++ components/shared/nav.tsx
@@ -1,11 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { ThemeSwitch } from "@/components/theme";
+import { ConfettiButton } from "@/components/ui/confetti";
+import { useEffect, useState } from "react";
 
 export function Nav() {
+  const pathname = usePathname();
+  const [isHomePage, setIsHomePage] = useState(false);
+
+  useEffect(() => {
+    setIsHomePage(pathname === "/");
+  }, [pathname]);
+
   return (
     <nav className="border-muted-foreground top-0 flex justify-between border-b py-4">
-      <Link href="/">jackey yang</Link>
+      {isHomePage ? (
+        <ConfettiButton
+          className="hover:text-secondary h-fit cursor-pointer bg-transparent p-0 text-base font-normal hover:bg-transparent"
+          options={{
+            get angle() {
+              return 295;
+            },
+          }}
+        >
+          jackey yang
+        </ConfettiButton>
+      ) : (
+        <Link href="/">jackey yang</Link>
+      )}
       <div className="flex items-center space-x-2">
         <Link href="/blog" className="mr-4">
           blog
